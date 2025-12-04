ff2c4483902ce69ec5b6927c0640bf80b9b97269
Tomide Adeyanju <tadeyanju@infinion.co>
2025-12-04 10:18:46 +0100

    added changes to the repository

--- GreshFi/src/components/Dashboard/DashboardWrapper.tsx
+++ GreshFi/src/components/Dashboard/DashboardWrapper.tsx
@@ -37,8 +37,8 @@ const DashboardWrapper = ({children}: React.PropsWithChildren) => {
   ]
 
   return (
-    <div className='h-screen bg-gradient-to-b from-[#0D2F28] to-[#33FFC2]'>
-      <div className='flex justify-between p-[20px] w-screen pt-[24px]'>
+    <div className='h-screen bg-gradient-to-b from-[#0D2F28] to-[#33FFC2] flex flex-col'>
+      <div className='flex justify-between p-[20px] w-screen pt-[24px] flex-shrink-0'>
         <div className='rounded-full w-[40px] h-[40px] bg-[#33FFC2] p-[8px] flex justify-center items-center'>
           <p className='font-semibold text-[16px]'>TA</p>
           </div> 
@@ -46,8 +46,8 @@ const DashboardWrapper = ({children}: React.PropsWithChildren) => {
          <img src={plus} alt="GreshPlus" />
           </div> 
       </div>
-      <main className='p-[20px]'>{children}</main>
-      <div className='absolute bottom-0'> 
+      <main className='p-[20px] flex-1 overflow-y-auto pb-20'>{children}</main>
+      <div className='flex-shrink-0'> 
       <div className='bg-[#124F40] w-screen grid grid-cols-4'>
         {profileLinks.map((link) => {
           return (
