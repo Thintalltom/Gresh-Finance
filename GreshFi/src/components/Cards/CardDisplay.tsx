e763134ecb31b3f7bfc53c5fee899f74329f30f7
Tomide Adeyanju <tadeyanju@infinion.co>
2025-12-04 10:02:43 +0100

    added changes to the repo

--- GreshFi/src/components/Cards/CardDisplay.tsx
+++ GreshFi/src/components/Cards/CardDisplay.tsx
@@ -11,7 +11,7 @@ const CardDisplay = ({ brandName, cardholderName, cardLogo }: CardDisplayProps) 
   return (
     <div className='flex justify-center items-center h-fit px-6'>
       <div className='min-w-[358px] rounded-[16px] p-[16px] min-h-[240px] bg-gradient-to-l flex flex-col justify-between from-[#9C56FF] to-[#0E0D2F] mt-[40px]'>
-        <p className="font-extrabold text-[32px] text-red-500">{brandName || 'Gresh'}</p>
+        <p className="font-extrabold text-[32px] text-white">{brandName || 'Gresh'}</p>
 
         <div className='flex justify-between'>
           <p className="text-white font-semibold text-[14px]">{cardholderName || 'CARDHOLDER NAME'}</p>
